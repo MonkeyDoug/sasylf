a63e93b409cefa0f8f654e9e0a1e1508466c3704
Douglas Chen <dchen3@andrew.cmu.edu>
2024-01-17 11:54:55 -0500

    added support for top down and bottom up

--- lsp-sasylf/electron_app/src/components/input.tsx
+++ lsp-sasylf/electron_app/src/components/input.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
 import Modal from "react-bootstrap/Modal";
 import { input } from "../types";
 
@@ -15,19 +12,39 @@ interface InputProps {
 }
 
 export default function Input(props: InputProps) {
+	const [selectedType, setSelectedType] = React.useState<string>("Premises");
+	const [numInputs, setNumInputs] = React.useState<number>(1);
+
+	const handleTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSelectedType(event.target.value);
+	};
+
 	const handleExport: React.FormEventHandler<HTMLFormElement> = (event) => {
 		event.preventDefault();
 		const formData = new FormData(event.target as HTMLFormElement);
 
 		const formJson = Object.fromEntries(formData.entries());
-		const input: string = formJson.input as string;
 		const free: boolean = formJson.hasOwnProperty("free");
+		const type: string = formJson.type as string;
+		let input: string[] = Array(numInputs).fill("");
+		if (type === "Conclusion") {
+			input[0] = formJson.input as string;
+		}
+		if (type === "Premises") {
+			for (const [key, value] of formData.entries()) {
+				let index = 0;
+				if (key.includes("input")) {
+					index = parseInt(key.replace("input-", ""), 10);
+					input[index] = value as string;
+				}
+			}
+		}
 		props.appendHandler({
-			input,
+			input: input.join(", "),
 			free,
 			id: Math.max(-1, ...props.inputs.map((element) => element.id)) + 1,
 		});
-		const inputType: string = formJson.type as string;
+		console.log(formJson);
 		props.onHide();
 	};
 
@@ -38,20 +55,60 @@ export default function Input(props: InputProps) {
 			</Modal.Header>
 			<Modal.Body>
 				<Form method="post" onSubmit={handleExport}>
-					<InputGroup className="mb-3">
-						<DropdownButton variant="outline-secondary" title="Dropdown">
-							<Dropdown.Item href="#">Conclusion</Dropdown.Item>
-							<Dropdown.Item href="#">Premises</Dropdown.Item>
-						</DropdownButton>
-						<Form.Control />
-					</InputGroup>
+					<Form.Check
+						type="radio"
+						label="Premises"
+						name="type"
+						value="Premises"
+						checked={selectedType === "Premises"}
+						onChange={handleTypeChange}
+					/>
+					<Form.Check
+						type="radio"
+						label="Conclusion"
+						name="type"
+						value="Conclusion"
+						checked={selectedType === "Conclusion"}
+						onChange={handleTypeChange}
+					/>
+
+					{selectedType === "Premises" && (
+						/* Render Premises-specific form elements */
+						<>
+							{Array.from(Array(numInputs).keys()).map((i) => (
+								<Form.Control
+									name={`input-${i}`}
+									type="text"
+									placeholder="Enter premise"
+									className="m-1"
+								/>
+							))}
+							<Button
+								variant="success"
+								className="m-1"
+								onClick={() => setNumInputs(numInputs + 1)}
+							>
+								Add Premise
+							</Button>
+						</>
+					)}
+
+					{selectedType === "Conclusion" && (
+						/* Render Conclusion-specific form elements */
+						<Form.Control
+							name="input"
+							type="text"
+							placeholder="Enter conclusion"
+						/>
+					)}
+
 					<Form.Check
 						type="switch"
 						name="free"
 						label="Allow free variables"
 						className="m-1"
 					/>
-					<Button variant="success" type="submit">
+					<Button variant="success" type="submit" size="lg">
 						Create
 					</Button>
 				</Form>
